5c19c451ff7fdbcadc22bac2585d38492abc54de
Thomas Weber <muffin@muffin.ink>
2021-12-31 18:43:08 -0600

    Remove setInterval from fetch worker
    It was probably perfectly fine, but it makes me uncomfortable

--- src/FetchWorkerTool.worker.js
+++ src/FetchWorkerTool.worker.js
@@ -1,19 +1,13 @@
 /* eslint-env worker */
 
-let jobsActive = 0;
 const complete = [];
 
-let intervalId = null;
+let timeoutId = null;
 
-/**
- * Register a step function.
- *
- * Step checks if there are completed jobs and if there are sends them to the
- * parent. Then it checks the jobs count. If there are no further jobs, clear
- * the step.
- */
-const registerStep = function () {
-    intervalId = setInterval(() => {
+const checkCompleted = () => {
+    if (timeoutId) return;
+    timeoutId = setTimeout(() => {
+        timeoutId = null;
         if (complete.length) {
             // Send our chunk of completed requests and instruct postMessage to
             // transfer the buffers instead of copying them.
@@ -30,11 +24,7 @@ const registerStep = function () {
             );
             complete.length = 0;
         }
-        if (jobsActive === 0) {
-            clearInterval(intervalId);
-            intervalId = null;
-        }
-    }, 1);
+    });
 };
 
 /**
@@ -42,17 +32,11 @@ const registerStep = function () {
  * @param {object} options.job A job id, url, and options descriptor to perform.
  */
 const onMessage = ({data: job}) => {
-    if (jobsActive === 0 && !intervalId) {
-        registerStep();
-    }
-
-    jobsActive++;
-
     fetch(job.url, job.options)
         .then(response => response.arrayBuffer())
         .then(buffer => complete.push({id: job.id, buffer}))
         .catch(error => complete.push({id: job.id, error}))
-        .then(() => jobsActive--);
+        .then(checkCompleted);
 };
 
 if (self.fetch) {
